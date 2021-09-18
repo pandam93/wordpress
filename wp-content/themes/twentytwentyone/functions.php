4fb9dca6006f86acc94b50b1b9feb52b86ad26dd
Carlos Millan <cfmillanm@gmail.com>
2021-09-18 19:41:46 +0200

    test2

--- wp-content/themes/twentytwentyone/functions.php
+++ wp-content/themes/twentytwentyone/functions.php
@@ -652,20 +652,3 @@ function twentytwentyone_add_ie_class()
 <?php
 }
 add_action('wp_footer', 'twentytwentyone_add_ie_class');
-
-/**
- * Register a custom menu page.
- */
-function wpdocs_register_my_custom_menu_page()
-{
-	add_menu_page(
-		__('Custom Menu Title', 'textdomain'),
-		'custom menu',
-		'manage_options',
-		'myplugin/myplugin-admin.php',
-		'',
-		plugins_url('myplugin/images/icon.png'),
-		6
-	);
-}
-add_action('admin_menu', 'wpdocs_register_my_custom_menu_page');
