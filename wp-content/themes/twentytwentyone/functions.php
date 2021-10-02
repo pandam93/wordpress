e86844cbfdcc329e9e19a597b8df90dd2edba724
Carlos Millan <cfmillanm@gmail.com>
2021-10-02 20:25:18 +0200

    dynamic routes

--- wp-content/themes/twentytwentyone/functions.php
+++ wp-content/themes/twentytwentyone/functions.php
@@ -652,3 +652,22 @@ function twentytwentyone_add_ie_class()
 <?php
 }
 add_action('wp_footer', 'twentytwentyone_add_ie_class');
+
+add_action('init', function() {
+	add_rewrite_rule( 'characters/[a-z]+[/]?$', 'index.php?character=$matches[1]', 'top' );
+});
+
+add_filter( 'query_vars', function($query_vars) {
+
+	$query_vars[] = 'character';
+	return $query_vars;
+});
+
+add_action( 'template_include', function( $template ) {
+
+	if (get_query_var('character') == false || get_query_var('character') == '' ) {
+		return $template;
+	}
+
+	return get_template_directory(). '/character.php';
+});
